3f3d2a5299d4357a0136a70ff51e625a6b25a558
Ugwu Friday B <fugwungene@gmail.com>
2022-10-13 15:41:03 -0700

    C programming

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#nclude <stdio.h>
+
+
+/* 
+ * Print the srting "programing is like building a multilingual puzzle"
+ * return 0
+ * /
+
+int main (void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return(0);
+}
