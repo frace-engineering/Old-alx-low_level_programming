a0f3bb8cfeac4d1f527c3e5f7d4b98edfcf0b002
Ugwu Friday B <fugwungene@gmail.com>
2022-10-14 18:02:20 -0700

    Variables

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+
+
+/*
+ * main - function that detects and prints the sign of the result of an expression
+ *return: always 0 (Success)
+ */
+
+int main(void)
+{
+	int n;
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if(n > 0)
+		printf("%f is positive\n", n);
+	else if(n < 0)
+		printf("%f is negative\n", n);
+	else
+		printf("%f is zero\n", n);
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/README.md
@@ -0,0 +1,1 @@
+0-positive_or_negative.c is a c program that reads out the result of arithmetic operation and the sign.
