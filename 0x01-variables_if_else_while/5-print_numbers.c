c1fa45d1d16246f50121ad8736194715356818c2
Ugwu Friday B <fugwungene@gmail.com>
2022-10-15 06:59:15 -0700

    Working on numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - This program prints out all the single digit unmbers of base 10 starting from 0
+ * return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int num;
+
+	for(num = 0; num < 10; num++)
+	{
+		printf("%d\n", num);
+	}
+	return(0);
+}
---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Print all single digit numbers of base 10 starting from 0 using only putchar fn
+ * return: Always 0 (success)
+ */
+
+
+int main(void)
+{
+	int num = 0;
+
+	while (num < 10)
+	{
+		putchar(num);
+	}
+	putchar('\n');
+}
