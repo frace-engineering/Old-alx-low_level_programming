5b332f9d091e9c7dc27d10100af7f2e79ca2348b
Ugwu Friday B <fugwungene@gmail.com>
2022-10-15 22:15:01 -0700

    Added optional task

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - get all possible combinations of two digits
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i;
+	int j;
+
+	for (i = 48; i <= 53; i++)
+	{
+		for (j = 52; j <= 58; j++)
+		{
+			if (j > i)
+			{
+				putchar(i);
+				putchar(j);
+				if (i != 53 || j !=58)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,40 @@
+
+#include <stdio.h>
+
+/**
+ * main - get all possible combinations of two digits
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i;
+	int j;
+	int k;
+
+	for (i = 48; i <= 53; i++)
+	{
+		for (j =46; j <= 58; j++)
+		{
+			for (k =54; k < 58; k++)
+			{
+				
+				if (k > j && j > i)
+				{
+					putchar(i);
+					putchar(j);
+					putchar(k);
+					if (i != 53 || j !=58)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+			
+			
+		
+	}
+	putchar('\n');
+	return (0);
+}
