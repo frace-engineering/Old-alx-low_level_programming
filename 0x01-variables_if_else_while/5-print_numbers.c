3c5fb79afb46d878288c62e860f061b578cc06a2
Ugwu Friday B <fugwungene@gmail.com>
2022-10-16 09:38:41 -0700

    A little hard

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -10,6 +10,6 @@ int main(void)
 	int num;
 
 	for (num = 0; num < 10; num++)
-		printf("%d\n", num);
+		printf("%d", num);
 	return (0);
 }
--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -12,8 +12,7 @@ int main(void)
 	for (num = 48; num < 59; num++)
 	{
 		putchar(num);
-		putchar('\n');
-		num++;
 	}
+	putchar('\n');
 	return (0);
 }
