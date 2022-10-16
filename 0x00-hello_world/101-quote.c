0f892cc81f70b5296a63629d5d91114a35aee883
Ugwu Friday B <fugwungene@gmail.com>
2022-10-16 15:38:36 -0700

    Add the correction

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <tring.h>
 
 /**
  * main - this prints exactly "and that piece of art is useful"
  * - Dora Korpar, 2015-10-19,"
- * Return: 1
+ * Return: 0
  */
 int main(void)
 {
 	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19,");
-	return (1);
+	return (0);
 }
 
--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
-
+#include <string.h>
 
 /**
- * main - Prints the srting using puts
- * Return: Always 0 (Success)
+ * main - print out string character with puts
+ * Return: 0 if successfull
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
