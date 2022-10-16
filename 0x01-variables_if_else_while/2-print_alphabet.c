227cd4d11c9ab19be58241a04609e1595e7cea08
Ugwu Friday B <fugwungene@gmail.com>
2022-10-15 20:51:11 -0700

    Print lowercase alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
-/** main - this function prints out the alphabets 
+/** 
+ * main - this function prints out the alphabets 
  * from a to z in lower case
  * return: Always 0
  */
