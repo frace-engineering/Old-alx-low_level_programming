a9dec6bea8e168289fba171e5dbc510b349b3de7
Ugwu Friday B <fugwungene@gmail.com>
2022-10-20 06:12:16 -0700

    Add task six

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include "main.h"
+
+
+/**
+ * _abs - prints the absolute value on an integer
+ * @n: the integer value to work on
+ * Return: absolute value on int n (Success)
+ */
+
+int _abs(int n)
+{
+	return (abs(n));
+}
+
