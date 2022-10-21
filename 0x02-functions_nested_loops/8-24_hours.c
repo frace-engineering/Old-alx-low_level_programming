40609de1ea57e19a774999a33eadc4c6622969f9
Ugwu Friday B <fugwungene@gmail.com>
2022-10-21 07:27:46 -0700

    Add task eight

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -28,11 +28,11 @@ void jack_bauer(void)
 
 				for (l = 0; l < 10; l++)
 				{
-					_putchar(i);
-					_putchar(j);
+					_putchar('i');
+					_putchar('j');
 					_putchar(':');
-					_putchar(k);
-					_putchar(l);
+					_putchar('k');
+					_putchar('l');
 				}
 				_putchar('\n');
 			}
