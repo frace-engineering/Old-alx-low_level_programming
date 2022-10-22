a178e66ae6a7e63752ca604a3731f74c9f4a8e55
Ugwu Friday B <fugwungene@gmail.com>
2022-10-22 07:44:14 -0700

    Add task two

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,10 +1,11 @@
 #ifndef MAIN_H
 #define main_H
 
-#include <stdio>
+#include <stdio.h>
 
 void postitive_or_negative(int i);
 
+int largest_number(int a, int b, int c);
 void print_remaining_days(void);
 
 #endif
