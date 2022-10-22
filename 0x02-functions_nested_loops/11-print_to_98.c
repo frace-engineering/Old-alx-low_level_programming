b3853ae59baa7244ccefdd19c2900102a43e9748
Ugwu Friday B <fugwungene@gmail.com>
2022-10-22 12:26:48 -0700

    Add task eleven

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * print_to_98 - prints all natural numbers from a give number to 98
+ * @n: input number
+ * Return: 0
+ */
+
+void print_to_98(int n)
+{
+	int i;
+
+	for (i = n; i <= 98; i++)
+	{
+		printf("%d,  ", i);
+	}
+	for (i = n; i >= 98; i--)
+	{
+		printf("%d, ", i);
+	}
+
+	return;
+}
