ae2b662dcab2f2eb5390165b28ad24f6804d71ca
Ugwu Friday B <fugwungene@gmail.com>
2022-10-22 13:50:22 -0700

    Add first optional task

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+
+/**
+ * print_times_table - prints times table nth number
+ * @n: number to be worked on
+ * Return: void
+ */
+void print_times_table(int n)
+{
+	int i, j, product;
+
+	if (n > 15 || n < 0)
+	{
+		return;
+	}
+	else
+	{
+	for (i = 0; i <= n; i++)
+	{
+		_putchar('0');
+		for (j = 1; j <= n; j++)
+		{
+			_putchar(',');
+			_putchar(' ');
+			product = i * j;
+			if (product >= 100)
+			{
+				_putchar((product / 100) + '0');
+				_putchar(((product % 100) / 10) + '0');
+				_putchar(((product % 100) % 10) + '0');
+			}
+			else if (product >= 10)
+			{
+			_putchar(product / 10 + '0');
+			_putchar(product % 10 + '0');
+			}
+			else
+			{
+				_putchar(product + '0');
+			}
+		}
+	}
+	}
+}
