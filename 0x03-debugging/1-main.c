aee98537cd654dc2cd894c59a9521e9e9422b777
Ugwu Friday B <fugwungene@gmail.com>
2022-10-22 05:51:06 -0700

    Add task one

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 
-
-
 /**
  * main - causes an infinite loop
  * Return: 0
  */
+
 int main(void)
 {
 	int i;
@@ -21,5 +20,5 @@ int main(void)
 	printf("Infinte loop avoided! \\0/\n");
 
 	return (0);
-}
 
+}
