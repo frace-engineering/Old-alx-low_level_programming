5cd1c1a6d4739ac085feedc09609e9ef28b4d809
Ugwu Friday B <fugwungene@gmail.com>
2022-10-23 13:50:25 -0700

    Add optional task

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -29,15 +29,18 @@ void print_times_table(int n)
 				_putchar((product / 100) + '0');
 				_putchar(((product % 100) / 10) + '0');
 				_putchar(((product % 100) % 10) + '0');
+				_putchar('\n');
 			}
 			else if (product >= 10)
 			{
 			_putchar(product / 10 + '0');
 			_putchar(product % 10 + '0');
+			_putchar('\n');
 			}
 			else
 			{
 				_putchar(product + '0');
+				_putchar('\n');
 			}
 		}
 	}
