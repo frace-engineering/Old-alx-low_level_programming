7cbcd278a43f23c9c6d03acbd7a7bd2c0bc4182b
Ugwu Friday B <fugwungene@gmail.com>
2022-10-23 11:23:39 -0700

    Add task eleven

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,14 +10,26 @@ void print_to_98(int n)
 {
 	int i;
 
-	for (i = n; i < 98; i++)
+	for (i = n; i <= 98; i++)
 	{
-		printf("%d, ", i);
-		printf("%d\n", n);
+		if (i != 98)
+		{
+			printf("%d, ", i);
+		}
+		else if (i == 98)
+		{
+			printf("%d\n", i);
+		}
 	}
-	for (i = n; i > 98; i--)
+	for (i = n; i >= 98; i--)
 	{
-		printf("%d, ", i);
-		printf("%d\n", n);
+		if (i >= 98)
+		{
+			printf("%d, ", i);
+		}
+		else if
+		{
+			printf("%d\n", i);
+		}
 	}
 }
