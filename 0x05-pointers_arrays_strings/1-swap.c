48b3f26f1c4c2ea4a886b9a17e79f8dc58adfc72
Ugwu Friday B <fugwungene@gmail.com>
2022-10-25 08:56:15 -0700

    Add task one

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -10,6 +10,21 @@
 
 void swap_int(int *a, int *b)
 {
-	a = &b;
-	b = &a;
+	int p;
+	int q;
+	int *ptp;
+	int *ptq;
+
+	ptp = &p;
+	ptq = &q;
+
+	if (*ptp = p)
+	{
+		*ptp = q;
+	}
+	else if (*ptq = q)
+	{
+		*ptq = p;
+	}
 }
+
