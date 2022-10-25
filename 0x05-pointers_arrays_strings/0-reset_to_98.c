56501bc70d4edfaabacd3ba9ad8a7a8bf677e25a
Ugwu Friday B <fugwungene@gmail.com>
2022-10-25 04:53:33 -0700

    Add task zero

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - updates an int varible to 98
+ * @n: is the pointer variable
+ * Return: Always 0
+ */
+void reset_to_98(int *n)
+{
+	int *check;
+       *check = 98;
+}
