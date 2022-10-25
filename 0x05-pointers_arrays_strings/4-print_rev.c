0e364b88877a72db460e9ea7de0558cfd74da433
Ugwu Friday B <fugwungene@gmail.com>
2022-10-25 13:38:00 -0700

    Add task four

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_rev - prints string in reverse order
+ * @s: input string
+ * Return: void
+ */
+
+void print_rev(char *s)
+{
+	int count;
+
+	count = 0;
+	while (count >= 0)
+	{
+		if (s[count] == '\0')
+		{
+			break;
+		}
+		count++;
+	}
+	for (; count >= 0; count--)
+	{
+		_putchar(s[count]);
+	}
+	_putchar('\n');
+}
