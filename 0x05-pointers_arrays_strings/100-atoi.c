e5f4894ed704322dcf6a9108baaeb12c4515d0ae
Ugwu Friday B <fugwungene@gmail.com>
2022-10-25 14:51:37 -0700

    Add advanced task one

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,3 +1,4 @@
+
 #include "main.h"
 
 /**
@@ -7,46 +8,22 @@
  */
 int _atoi(char *s)
 {
-	int a, b, c, d, length, digit;
+	int i, d, n, f, len, digit;
 
-	a = 0;
-	b = 0;
-	c = 0;
+	i = 0;
 	d = 0;
-	length = 0;
+	n = 0;
+	f = 0;
+	len = 0;
 	digit = 0;
 
-	while (s[length] != '\0')
+	while (s[len] != '\0')
+		len++
+	while (i < len && f == 0)
 	{
-		length++
-	}
-
-	while (a < length && d == 0)
-	{
-		if (s[a] == '-')
+		if (s[i] == '-')
 		{
-			++b;
-			if (s[a] >= '0' && s[a] <= '9')
-			{
-				digit = s[a] - '0';
-				if (b % 2)
-				{
-					digit = -digit;
-				}
-				c = c * 10 + digit;
-				d = 1;
-				if (s[a + 1] < '0' || s[a + 1] > '9')
-					break;
-				d = 0;
-			}
-			a++;
-		}
-		if (d == 0)
-		{
-			return (0);
-		}
-	}
-	return (c);
-}
+			++d;
 
-
+		if (s[i] >= '0' && s[i] <= '9')
+		digit = s[i] - '0';
