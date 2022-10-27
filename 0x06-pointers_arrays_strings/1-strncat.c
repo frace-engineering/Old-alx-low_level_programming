890db7367bf4c3c2f975f99d6ae4a831caee44b1
Ugwu Friday B <fugwungene@gmail.com>
2022-10-27 09:59:40 -0700

    Add task one

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * *_strncat - appends a determined number of  char from src string
+ * to destinating string without a NULL terminator
+ * @dest: destination string
+ * @src: source string
+ * @n: number of character to be appended from src
+ * Return: pointer
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, j;
+
+	while (dest[i++] != '\0')
+	{
+		_putchar(dest[i]);
+		if (dest[i] == '\0')
+		{
+			for (j = 0; j <= n; j++)
+			{
+				_putchar(src[j]);
+			}
+		}
+		_putchar('\n');
+	}
+	return (dest);
+}
+
