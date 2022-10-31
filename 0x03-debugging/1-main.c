521c13726cb83d1b11d46b665a554f9f42da98ed
Ugwu Friday B <fugwungene@gmail.com>
2022-10-30 18:40:33 -0700

    Add task one

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -13,10 +13,12 @@ int main(void)
 
 	i = 0;
 
-	/*while (1 < 10)*/
-	{
-		putchar(i);
-	}
+	/*while (1 < 10)
+	*{
+	*	putchar(i);
+	*}
+	*/
+
 	printf("Infinte loop avoided! \\0/\n");
 
 	return (0);
