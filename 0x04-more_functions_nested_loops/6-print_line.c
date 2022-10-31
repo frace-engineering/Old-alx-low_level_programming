719b2a41d1432073dc263630abfde35990c286e0
Ugwu Friday B <fugwungene@gmail.com>
2022-10-30 22:33:20 -0700

    Add task six

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -10,10 +10,16 @@ void print_line(int n)
 	int i;
 	
 	if (n <= 0)
+	{
 		_putchar('\n');
-	break;
-	for (i = 0; i < n; i++)
+	}
+	else
+	
+	for (i = 1; i < n; i++)
+	{
 		_putchar('_');
+	}
+	_putchar('\n');
 	return;
 }
 	
