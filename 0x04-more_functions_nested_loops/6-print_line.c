67a0b48a01a466e4ab6a666212d1f12166bd90e7
Ugwu Friday B <fugwungene@gmail.com>
2022-10-30 22:27:23 -0700

    Add task six

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,10 +1,20 @@
 #include "main.h"
 
 /**
- *
- *
- *
+ * print_line - prints a straight line on the terminal
+ * @n: number of times _ should be printed
+ * Return: void
  */
 void print_line(int n)
 {
-	for (n
+	int i;
+	
+	if (n <= 0)
+		_putchar('\n');
+	break;
+	for (i = 0; i < n; i++)
+		_putchar('_');
+	return;
+}
+	
+			
