83a1a0cca4de07deee453b20b6cab65f2b962560
Ugwu Friday B <fugwungene@gmail.com>
2022-11-01 07:09:08 -0700

    Add task zero

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * *_memset - fills the pointed memory witha given character
+ * @b: the character to fill the memory aerea with.
+ * @n: the number of times the character will be  registered
+ * @s: the string to be worked on
+ *
+ * Return: pointer
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i;
+
+	i = 0;
+
+	while (i < n)
+	{
+		s[i] = 'b';
+		i++;
+	}
+	return (s);
+}
+
