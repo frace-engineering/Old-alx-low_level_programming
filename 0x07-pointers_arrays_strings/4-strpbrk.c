6d8c9c256ea0fb838acd3e0cf42ccf5552c1b4d2
Engr Ugwu F.B <fugwungene@gmail.com>
2022-11-02 01:20:46 +0000

    Add task four

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strpbrk - searches a string for any of a set of bytes.
+ * @s: pointer to string
+ * @accept: substring to search for in the string s
+ *
+ * Return: the length of the substring..
+*/
+char *_strpbrk(char *s, char *accept)
+{
+	char *ptr = strpbrk(s, accept); 
+
+	return (ptr);
+}
