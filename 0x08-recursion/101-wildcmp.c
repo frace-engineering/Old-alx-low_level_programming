c0d8cf1ce233fbd5fa6cb8874a7e67fbe3b6b8c9
Engr Ugwu F.B <fugwungene@gmail.com>
2022-11-03 17:00:18 +0000

    Add advanced task two

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,28 +1,24 @@
 #include "main.h"
 
 /**
- * wildcmp - compares two string.
- * @s1: second string
- * @s2: second string
- * Return: 1 if strings are identical else 0.
+ * wildcmp - Compare two strings allowing for wildcard char
+ * @s1: String being compared
+ * @s2: String being compared against
+ *
+ * Return: 1 if considered identical, 0 otherwise
  */
-
 int wildcmp(char *s1, char *s2)
 {
-	if (*s1 != '\0' && *s2 != '\0')
-	{
-	if (*s1 == *s2)
-	{
+	if (*s1 == '\0' && *s2 == '\0')
 		return (1);
-		s1++;
-		s2++;
-		wildcmp(s1, s2);
-	}
-	else
+	if (*s1 == *s2)
+		return (wildcmp(s1 + 1, s2 + 1));
+	if (*s2 == '*')
 	{
-		return (0);
+		if (*(s2 + 1) == '*')
+			return (wildcmp(s1, s2 + 1));
+		if (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1))
+			return (1);
 	}
-	}
+	return (0);
 }
-
-
