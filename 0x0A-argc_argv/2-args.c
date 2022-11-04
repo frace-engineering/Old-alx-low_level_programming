31bf9acd18d27054461b31c48ebe4659682a262d
Engr Ugwu F. B <fugwungene@gmail.com>
2022-11-04 22:06:39 +0000

    Add task two

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -2,12 +2,12 @@
 #include <stdio.h>
 
 /**
- * print_arg - prints the arguments of a program
+ * args_list - is s function that prints its name
  * @a: number of elements in array b
  * @b: array of arguments
  * Return: Always 0 (Success)
  */
-int print_arg(int a, char *b[])
+int args_list(__attribute__((unused)) int a, char *b[])
 {
 	int i;
 
@@ -17,8 +17,14 @@ int print_arg(int a, char *b[])
 	}
 	return (0);
 }
-
+/**
+ * main - entry point for the program
+ * @argc: argument count
+ * @argv: parameter vector
+ * Return: Always 0 (Success)
+ */
 int main(int argc, char *argv[])
 {
-	print_arg(argc, argv);
+	args_list(argc, argv);
+	return (0);
 }
