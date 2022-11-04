6e6dd9c0fafbc4f311de7891daf7ecb3577610d6
Engr Ugwu F. B <fugwungene@gmail.com>
2022-11-04 21:53:24 +0000

    Add task one

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -2,18 +2,24 @@
 #include <stdio.h>
 
 /**
- * arg_count - counts the number of arguments in a function
+ * args_count - is s function that prints its name
  * @a: number of elements in array b
  * @b: array of arguments
  * Return: Always 0 (Success)
  */
-int args_count(int a, char *b[])
+int args_count(__attribute__((unused)) int a, char *b[])
 {
 	printf("%d\n", a);
 	return (0);
 }
-
+/**
+ * main - entry point for the program
+ * @argc: argument count
+ * @argv: parameter vector
+ * Return: Always 0 (Success)
+ */
 int main(int argc, char *argv[])
 {
 	args_count(argc, argv);
+	return (0);
 }
