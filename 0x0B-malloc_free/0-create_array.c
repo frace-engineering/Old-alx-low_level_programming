98c4b1727440a1bc7a6b16c50a936c92cfc9cef7
Engr Ugwu F. B <fugwungene@gmail.com>
2022-11-08 14:49:55 +0000

    Add task zero

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -10,22 +10,23 @@
 
 char *create_array(unsigned int size, char c)
 {
-	int i;
+	unsigned int i;
 	char *ptr;
 
+	i = 0;
+	
 	ptr = malloc(sizeof(char) * size);
 	
 	if (size == 0)
 	{
 		return ('\0');
 	}
-	for (i = 0; i < size; i++)
+	while (i < size)
 	{
 		_putchar(c);
+		i++;
 	}
 	return (ptr);
-
-
 }
 
 
