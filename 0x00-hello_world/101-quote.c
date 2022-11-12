ef8b814c4690e4e40a5a59c107d42e318bfd7305
Engr Ugwu F. B <fugwungene@gmail.com>
2022-11-12 17:10:06 +0000

    Add advanced task correction

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,11 +5,11 @@
 /**
  * main - this prints exactly "and that piece of art is useful"
  * - Dora Korpar, 2015-10-19,"
- * Return: 0
+ * Return: 1
  */
 int main(void)
 {
-	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
-	return (0);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
 
