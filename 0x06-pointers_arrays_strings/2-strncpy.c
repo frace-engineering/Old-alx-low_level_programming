6cd426653cd102b26e1fa995bd11838c8020a6d0
Engr Ugwu F. B <fugwungene@gmail.com>
2022-11-14 00:57:42 +0000

    Add task two

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strncpy - copies nth length of a string to a destination
+ * @dest: destination buffer
+ * @src: string to be copied
+ * @n: number of characters to be copied from src
+ * Return: pointer to dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+	for (; i < n; i++)
+	{
+		dest[i] = '\0';
+	}
+	return (dest);
+}
