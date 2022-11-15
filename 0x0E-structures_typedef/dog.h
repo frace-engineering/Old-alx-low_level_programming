f0a82dfe97829a0cf24fdf9e60097046434cb87e
Engr Ugwu F. B <fugwungene@gmail.com>
2022-11-15 15:33:02 +0000

    Add taske zero

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * dog - structure name
+ * @name: structure member
+ * @age: structure member 2
+ * @owner: structure member 3
+ */
+struct dog{
+	char *name;
+	float age;
+	char *owner;
+}
