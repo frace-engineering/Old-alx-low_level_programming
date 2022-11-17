3a54e3417935b63adc3ef83af4adb2cac6057205
Engr Ugwu F. B <fugwungene@gmail.com>
2022-11-17 11:01:03 +0000

    Add task zero

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -6,8 +6,10 @@
  * @name: name to be printed
  * @f: function pointer that takes a string
  */
-
 void print_name(char *name, void (*f)(char *))
 {
-	f(name);
+	if (f != NULL)
+	{
+		f(name);
+	}
 }
