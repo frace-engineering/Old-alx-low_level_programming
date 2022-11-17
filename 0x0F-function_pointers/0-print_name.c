1d6b11339cad24972d08fc01e98870dd687b720c
Engr Ugwu F. B <fugwungene@gmail.com>
2022-11-17 11:03:45 +0000

    Addtask zero

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -5,6 +5,7 @@
  * print_name - prints a name given by the function
  * @name: name to be printed
  * @f: function pointer that takes a string
+ * Return: void
  */
 void print_name(char *name, void (*f)(char *))
 {
