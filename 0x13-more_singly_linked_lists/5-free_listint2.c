17201601e2bd003a640bd10cc838cb273a88f25e
Engr Ugwu Friday B <fugwungene@gmail.com>
2022-11-30 11:07:01 +0000

    Debugging task five

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -12,17 +12,12 @@ void free_listint2(listint_t **head)
 	listint_t *temp;
 	listint_t *see;
 
-	if (head == 0)
-	{
-		return;
-	}
-
 	temp = *head;
-	while (temp)
+	while (temp != NULL)
 	{
-		see = temp->next;
-		free(temp);
-		temp = see;
+		see = temp;
+		temp = temp->next;
+		free(see);
 	}
-	temp = NULL;
+	*head = NULL;
 }
