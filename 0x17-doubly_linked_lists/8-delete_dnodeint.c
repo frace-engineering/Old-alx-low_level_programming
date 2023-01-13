c8bcc7895fb2fefa74995e3d3ecd23ce63b9eae6
Engr Ugwu Friday B <fugwungene@gmail.com>
2023-01-13 23:51:53 +0000

    [free and return] in task eight

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -42,8 +42,12 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 		free(temp);
 		return (1);
 	}
-	temp->prev->next = temp->next;
-	temp->next->prev = temp->prev;
-	free(temp);
-	return (1);
+	else
+	{
+		temp->prev->next = temp->next;
+		temp->next->prev = temp->prev;
+		free(temp);
+		return (1);
+	}
+	return (-1);
 }
