bed3a0d23d0e1ecdbe8de5d1b36cc5d710562f2a
Engr Ugwu Friday B <fugwungene@gmail.com>
2023-01-13 23:34:38 +0000

    [Fix] bug in task eight

---
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,44 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * delete_dnodeint_at_index - add node at a given idex
+ * @head: pointer to head nod
+ * @index: given index
+ * Return: new_node pointer
+ */
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	dlistint_t *temp;
+	unsigned int count;
+
+	if ((*head) == NULL)
+		return (-1);
+	temp = *head;
+	count = 0;
+	while (temp != NULL && count  < index)
+	{
+		temp = temp->next;
+		count++;
+	}
+	if ((*head)->next == NULL)
+	{
+		*head = NULL;
+		return (1);
+	}
+	if (temp->next == NULL)
+	{
+		temp = temp->prev;
+		temp->prev->next = NULL;
+		return (1);
+	}
+	if (temp->prev== NULL)
+	{
+		*head = (*head)->next;
+		(*head)->prev = NULL;
+		return (1);
+	}
+	temp->prev->next = temp->next;
+	temp->next->prev = temp->prev;
+	return (1);
+}
