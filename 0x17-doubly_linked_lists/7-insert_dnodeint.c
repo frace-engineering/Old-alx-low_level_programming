858edbbcea26d357cc168ac1d3cdb1701975a6d7
Engr Ugwu Friday B <fugwungene@gmail.com>
2023-01-14 01:59:24 +0000

    Fix bugs in task seven

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -14,23 +14,36 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *temp;
 	unsigned int count;
 
+	count = 0;
 	new_node = malloc(sizeof(dlistint_t));
 	if (new_node == NULL)
 		return (NULL);
 	new_node->n = n;
 	if (*h == NULL)
 	{
-		return (0);
+		return (NULL);
 	}
 	temp = *h;
-	count =0;
-	while (temp != NULL && count < idx)
+	while (count < idx - 1)
 	{
 		temp = temp->next;
 		count++;
 	}
+	new_node->prev = temp;
 	new_node->next = temp->next;
 	temp->next = new_node;
-	temp = new_node;
+	if (temp->next == NULL)
+	{
+		new_node->prev = temp;
+		temp->next = new_node;
+		new_node->next = NULL;
+	}
+	if (temp->prev == NULL)
+	{
+		new_node->next = temp;
+		*h = new_node;
+		new_node->prev = NULL;
+	}
+
 	return (new_node);
 }
