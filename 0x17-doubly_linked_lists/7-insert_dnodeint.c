be5b1ce21af718aefac818e5d5e1e14732efa869
Engr Ugwu Friday B <fugwungene@gmail.com>
2023-01-17 15:44:44 +0000

    [Fixed the bug

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -15,7 +15,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	unsigned int count;
 
 	new_node = malloc(sizeof(dlistint_t));
-	if (new_node == NULL)
+	if (new_node == NULL || *h == NULL)
 	{
 		return (NULL);
 	}
